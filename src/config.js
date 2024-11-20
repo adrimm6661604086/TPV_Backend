dbf8880a4e61dc92bdbdfd43ef0021094b928e4e
Adria Martinez <adrimm666@gmail.com>
2024-11-20 16:37:51 +0100

    Environment variables

--- src/config.js
+++ src/config.js
@@ -1,15 +1,13 @@
-require('dotenv').config();
+import dotenv from 'dotenv';
+dotenv.config();
 
-module.exports = {
-    PORT: process.env.PORT || 3000,
-    BANK_API_URL: process.env.BANK_API_URL,
-    BANK_API_KEY: process.env.BANK_API_KEY,
-
-    DATABASE_URL: process.env.DATABASE_URL,
-    DB_HOST: process.env.DB_HOST,
-    DB_USER: process.env.DB_USER,
-    DB_PASSWORD: process.env.DB_PASSWORD,
-    DB_NAME: process.env.DB_NAME,
-    DB_PORT: process.env.DB_PORT,
-    DB_SERVER: process.env.DB_SERVER,
-};
+export const PORT = process.env.PORT || 5000;
+export const BANK_API_URL = process.env.BANK_API_URL;
+export const BANK_API_KEY = process.env.BANK_API_KEY;
+export const DATABASE_URL = process.env.DATABASE_URL;
+export const DB_HOST = process.env.DB_HOST;
+export const DB_USER = process.env.DB_USER;
+export const DB_PASSWORD = process.env.DB_PASSWORD;
+export const DB_NAME = process.env.DB_NAME;
+export const DB_PORT = process.env.DB_PORT;
+export const DB_SERVER = process.env.DB_SERVER;
