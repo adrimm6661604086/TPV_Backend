ce4fa2d3fecc7a8aa05dc246c981e5a71194be0f
Adria Martinez <adrimm666@gmail.com>
2024-11-29 18:39:45 +0100

    Conection to db

--- src/app.js
+++ src/app.js
@@ -1,15 +1,29 @@
+// Libraries
 import express from 'express';
-import { PORT } from './config.js';
+
+// Utils
 import router from './routes/index.js';
+import { PORT } from './config.js';
+import { connectDb } from './db.js'; 
 
 const app = express();
 
 app.use(express.json());
-
 app.use('/api', router);
 
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectDb();
+
+    app.listen(PORT, () => {
+      console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    });
+  } catch (err) {
+    console.error('Error al conectar con la base de datos:', err);
+    // process.exit(1); 
+  }
+};
+
+startServer();
 
 export default app;
