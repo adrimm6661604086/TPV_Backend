0b3f65269c2046d5f82df7db9e6d290b908293b2
Adria Martinez <adrimm666@gmail.com>
2024-11-29 18:37:46 +0100

    Logger added to routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import userRoutes from './UserRoutes.js';
 import transactionRoutes from './TransactionRoutes.js';
+import logger from '../logger.js';
 
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    logger.info('Acceso a la raíz de la API');
     res.json({ message: '¡Bienvenido a la API de TPVVirtual!' });
 });
 
