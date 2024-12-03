40482510aedcbf48d6c7d0300bb4f2f1eeeee5ee
Adria Martinez <adrimm666@gmail.com>
2024-12-03 02:30:53 +0100

    Unused routes

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -7,21 +7,11 @@ import UserController from '../controllers/UserController.js';
 
 const router = express.Router();
 
+
+
 router.post('/register-user', UserController.registerUser);
 
 router.post('/login-user', UserController.loginUser);
 
-
-router.get('/:id', (req, res) => {
-  try {
-    const user = UserController.getUserById(req.params.id);
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-    res.json(user);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
 
 export default router;
