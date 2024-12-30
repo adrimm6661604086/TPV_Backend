3bb12778e3074c5d3b591bdc65e46d13569e7213
Adria Martinez <adrimm666@gmail.com>
2024-12-30 12:05:13 +0100

    Test for register

---
+++ tests/registerTest.js
@@ -0,0 +1,15 @@
+const registerTest = {
+    "name": "John",
+    "lastName": "Doe",
+    "email": "john.doe@example.com",
+    "password": "password123",
+    "DNI": "12345678A",
+    "phoneNumber": "123456789",
+    "address": "123 Main St",
+    "postalCode": "12345",
+    "city": "City",
+    "country": "Spain",
+    "IBAN": "ES1234567890123456789012",
+    "bankEntity": "BankSim",
+    "account_type": "individual"
+  }
