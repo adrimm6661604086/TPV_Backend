09fe305cc4c3e5aa1b6a67d3101c44656d0649cd
Adria Martinez <adrimm666@gmail.com>
2025-01-07 23:08:47 +0100

    Fix route error

--- src/routes/TransactionRoutes.js
+++ src/routes/TransactionRoutes.js
@@ -4,11 +4,11 @@ import TransactionController from '../controllers/TransactionController.js';
 // api/transaction
 const router = express.Router();
 
+router.get('/:userId', TransactionController.getAllTransactionsFromAccount);
+
 router.post('/payment', TransactionController.payment);
 
 router.post('/return/:id', TransactionController.returnTransaction);
-
-router.get('/:accountId', TransactionController.getAllTransactionsFromAccount);
 
 
 export default router;
